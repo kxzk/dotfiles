9f6975a0f3f12d96ea7fed131a9c8a161851337a
kade killary <kadekillary@protonmail.com>
2018-05-30 00:13:41 -0700

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -10,60 +10,57 @@ sudo apt-get autoclean --yes
 
 # basic utils
 sudo apt-get install tree --yes
-sudo apt-get install parallel --yes
-sudo apt-get install shellcheck --yes
+# sudo apt-get install parallel --yes
+# sudo apt-get install shellcheck --yes
 
 # my colorscheme
-git clone https://github.com/kadekillary/subtle_solo.git
+# git clone https://github.com/kadekillary/subtle_solo.git
 
-mkdir .vim
-cd .vim/
-mkdir colors
-cd
+# mkdir .vim
+# cd .vim/
+# mkdir colors
+# cd
 
 # bash scripts
-git clone https://github.com/kadekillary/bashing.git
-echo 'echo PATH=~/bashing:$PATH' >> ~/.bashrc
+# git clone https://github.com/kadekillary/bashing.git
+# echo 'echo PATH=~/bashing:$PATH' >> ~/.bashrc
 
 # bring in my colors
-ln -sv -f /home/ubuntu/subtle_solo/colors/subtle_dark.vim /home/ubuntu/.vim/colors
-ln -sv -f /home/ubuntu/subtle_solo/colors/subtle_light.vim /home/ubuntu/.vim/colors
+# ln -sv -f /home/ubuntu/subtle_solo/colors/subtle_dark.vim /home/ubuntu/.vim/colors
+# ln -sv -f /home/ubuntu/subtle_solo/colors/subtle_light.vim /home/ubuntu/.vim/colors
 
 # my dotfiles
-git clone https://github.com/kadekillary/dotfiles.git
+# git clone https://github.com/kadekillary/dotfiles.git
 
-ln -sv -f /home/ubuntu/dotfiles/.vimrc /home/ubuntu
-ln -sv -f /home/ubuntu/dotfiles/.tmux.conf /home/ubuntu
-sed -i'' -e '24d' /home/ubuntu/.tmux.conf
-sed -i'' -e '27d' /home/ubuntu/.tmux.conf
-
-# pip3
-sudo apt-get install python3-pip --yes
-pip3 install --upgrade pip
+# ln -sv -f /home/ubuntu/dotfiles/.vimrc /home/ubuntu
+# ln -sv -f /home/ubuntu/dotfiles/.tmux.conf /home/ubuntu
+# sed -i'' -e '24d' /home/ubuntu/.tmux.conf
+# sed -i'' -e '27d' /home/ubuntu/.tmux.conf
 
 # python 3.6
 sudo add-apt-repository ppa:jonathonf/python-3.6 --yes
 sudo apt-get update
 sudo apt-get install python3.6 --yes
+# sudo python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall
 
 # vim 8
-sudo add-apt-repository ppa:jonathonf/vim --yes
-sudo apt update
-sudo apt-get install vim --yes
+# sudo add-apt-repository ppa:jonathonf/vim --yes
+# sudo apt update
+# sudo apt-get install vim --yes
 
 # rust
-curl https://sh.rustup.rs -sSf | sh
+# curl https://sh.rustup.rs -sSf | sh
 
 # go
-wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
-sudo tar -xvf go1.10.2.linux-amd64.tar.gz
-sudo mv go /usr/local
+# wget https://dl.google.com/go/go1.10.2.linux-amd64.tar.gz
+# sudo tar -xvf go1.10.2.linux-amd64.tar.gz
+# sudo mv go /usr/local
 
-'export GOROOT=/usr/local/go' >> ~/.profile
-'export GOPATH=$HOME/go-workspace' >> ~/.profile
-'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.profile
+# 'export GOROOT=/usr/local/go' >> ~/.profile
+# 'export GOPATH=$HOME/go-workspace' >> ~/.profile
+# 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.profile
 
-source ~/.profile
+# source ~/.profile
 
 # python packages
 pip3 install awscli --user
